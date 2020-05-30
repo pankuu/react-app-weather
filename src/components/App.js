5798bd34a175d2fcbdda8fe17ba477150565174e
marcinpank <marcin.pankiewicz@gmail.com>
2020-05-30 13:57:41 +0200

    App structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Form from "./Form";
+import Result from "./Result";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+        <Result />
+      </div>
+    );
+  }
+}
+
+export default App;
