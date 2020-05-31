c916c7aba33e5e5d7e7d983b9279c1ba917f0b94
marcinpank <marcin.pankiewicz@gmail.com>
2020-05-31 22:09:17 +0200

    Added env variable, localization api

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "./Form";
 import Result from "./Result";
+require("dotenv").config();
 
-const APIKey = "cab76c76bc09f5a7054d70423d3c98c1";
+const APIKey = process.env.REACT_APP_OPEN_WEATHER_MAP;
+const LOCATIONKey = process.env.REACT_APP_LOCATION_IQ;
 
 class App extends Component {
   state = {
@@ -25,47 +27,72 @@ class App extends Component {
     });
   };
 
-  handleCitySubmit = (e) => {
-    e.preventDefault();
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetch(
+        `https://us1.locationiq.com/v1/reverse.php?key=${LOCATIONKey}&lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response;
+          }
+          throw Error("Not found");
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({
+            value: data.address.city,
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            error: true,
+          });
+        });
+    });
+  }
 
-    fetch(API)
-      .then((response) => {
-        if (response.ok) {
-          return response;
-        }
-        throw Error("Not found:" + this.state.value);
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        const time = new Date().toLocaleString();
-        this.setState((prevState) => ({
-          date: time,
-          city: prevState.value,
-          sunrise: data.sys.sunrise,
-          sunset: data.sys.sunset,
-          temp: data.main.temp,
-          pressure: data.main.pressure,
-          wind: data.wind.speed,
-          error: false,
-        }));
-      })
-      .catch((error) => {
-        this.setState((prevState) => ({
-          error: true,
-          city: prevState.value,
-        }));
-      });
-  };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.value.length < 3) {
+      return;
+    }
+
+    if (prevState.value !== this.state.value) {
+      const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+      fetch(API)
+        .then((response) => {
+          if (response.ok) {
+            return response;
+          }
+          throw Error("Not found:" + this.state.value);
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          const time = new Date().toLocaleString();
+          this.setState((prevState) => ({
+            date: time,
+            city: prevState.value,
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset,
+            temp: data.main.temp,
+            pressure: data.main.pressure,
+            wind: data.wind.speed,
+            error: false,
+          }));
+        })
+        .catch((error) => {
+          this.setState((prevState) => ({
+            error: true,
+            city: prevState.value,
+          }));
+        });
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <Form
-          value={this.state.value}
-          change={this.handleInputChange}
-          submit={this.handleCitySubmit}
-        />
+        <Form value={this.state.value} change={this.handleInputChange} />
         <Result weather={this.state} />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Form = (props) => {
   return (
-    <form onSubmit={props.submit}>
+    <form>
       <input
         type="text"
         value={props.value}
         placeholder="City name"
         onChange={props.change}
       />
-      <button>Search</button>
+      {/* <button>Search</button> */}
     </form>
   );
 };
